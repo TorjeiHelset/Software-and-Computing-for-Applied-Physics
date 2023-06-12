84e3af34f2da88a9fb058ba98f793289b0a62368
TorjeiHelset <torjei.helset@gmail.com>
2023-06-12 12:31:38 +0200

    Added unit test to check that dimension of output intensity from simulate-photons

--- tests.py
+++ tests.py
@@ -8,17 +8,27 @@ class Test_simulate_photons(unittest.TestCase):
     """
     Class for testing the simulate photons function
     """
-    def test_intensity_decreasing(self):
+    def setUp(self):
         # Setting up parameters
-        n_photons = random.randint(10000, 100000)
-        n_steps = random.randint(100, 1000)
-        width = random.randint(5,20)
-        mu = random.random() * np.ones(n_steps)
-        intensity = sf.simulate_photons(n_photons, n_steps, width, mu)
+        self.n_photons = random.randint(10000, 100000)
+        self.n_steps = random.randint(100, 1000)
+        self.width = random.randint(5,20)
+        self.mu = random.random() * np.ones(self.n_steps)
+
+    def test_intensity_decreasing(self):
+        # Run simulation
+        intensity = sf.simulate_photons(self.n_photons, self.n_steps, self.width, self.mu)
 
         # Check that intensity is decreasing
         for i in range(len(intensity)-1):
             self.assertGreaterEqual(intensity[i], intensity[i+1])
+        
+    def test_intensity_length_correct(self):
+        # Run simulation
+        intensity = sf.simulate_photons(self.n_photons, self.n_steps, self.width, self.mu)
+
+        # Check that the outputed intensity has length equal to the number of steps + 1 (including start/end)
+        self.assertEqual(len(intensity), self.n_steps + 1)
 
 
 if __name__ == '__main__':
