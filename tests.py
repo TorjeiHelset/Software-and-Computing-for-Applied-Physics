ac4793b0e881311c90aae30cd2d72042cb048e88
TorjeiHelset <torjei.helset@gmail.com>
2023-08-04 16:44:33 +0200

    Added test to check that new simulate function gives similar results to before

--- tests.py
+++ tests.py
@@ -31,5 +31,28 @@ class Test_simulate_photons(unittest.TestCase):
         self.assertEqual(len(intensity), self.n_steps + 1)
 
 
+class simulate_photons_detector(unittest.TestCase):
+    """
+    Class for testing the simulate photons detector function
+    """
+    def setUp(self):
+        # Setting up parameters
+        self.n_photons = random.randint(10000, 100000)
+        self.n_steps = random.randint(100, 1000)
+        self.width = random.randint(5,20)
+        self.mu = random.random() * np.ones(self.n_steps)
+
+    def test_comparable_result(self):
+        # Checking that the intensity at the decode is similar to the one from other simulation
+        # approach
+
+        # Run simulation for both approaches
+        old_approach = sf.simulate_photons(self.n_photons, self.n_steps, self.width, self.mu)
+        target = old_approach[-1]
+        end_intensity = sf.simulate_photons_detector(self.n_photons, self.n_steps, self.width, self.mu)
+        
+        # Checking that the two intensities are "close enough"
+        self.assertAlmostEqual(target, end_intensity, delta = 0.001)
+
 if __name__ == '__main__':
     unittest.main()
