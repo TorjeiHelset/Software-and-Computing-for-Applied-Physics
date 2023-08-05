6af4cc53f60ca60948b78117fd673f76a248183d
TorjeiHelset <torjei.helset@gmail.com>
2023-08-05 12:24:34 +0200

    Initialised streamlit app

---
+++ xray_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import numpy as np # Numpy for handling arrays
+import matplotlib.pyplot as plt # Matplotlib to do the plotting
+import tabulate # Tables
+from numba import jit #just in time compilation - faster code
+
